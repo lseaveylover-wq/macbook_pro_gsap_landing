61021c9969b8e24cec180a83c148f4883ab353a1
l.seavey <l.seavey.lover@gmail.com>
2026-08-26 15:33:53 +0700

    Implement Footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,31 @@
+import { footerLinks } from "../constants";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer>
+      <div className="info">
+        <p>
+          More ways to shop: Find an Apple Store or other retailer near you. Or
+          get help choosing the perfect MacBook for you.
+        </p>
+        <img src="/logo.svg" alt="Apple logo" />
+      </div>
+      <hr />
+      <div className="links">
+        <p>
+          Copyright &copy; {new Date().getFullYear()} Apple Inc. All rights
+          reserved.
+        </p>
+        <ul>
+          {footerLinks.map(({ label, link }) => (
+            <li key={label}>
+              <a href={link}> {label} </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
